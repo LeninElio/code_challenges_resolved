23553428a6cf95d2012b65a372b2f790ca55badd
LeninElio <moreno_leninelio@hotmail.es>
2022-08-18 23:49:59 -0500

    Creando el diccionario de meses

---
+++ cuantos_dias/cuantos_dias.py
@@ -0,0 +1,39 @@
+"""
+
+/*
+ * Reto #15
+ * ¿CUÁNTOS DÍAS?
+ * Fecha publicación enunciado: 11/04/22
+ * Fecha publicación resolución: 18/04/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Crea una función que calcule y retorne cuántos días hay entre dos cadenas de texto que representen fechas.
+ * - Una cadena de texto que representa una fecha tiene el formato "dd/MM/yyyy".
+ * - La función recibirá dos String y retornará un Int.
+ * - La diferencia en días será absoluta (no importa el orden de las fechas).
+ * - Si una de las dos cadenas de texto no representa una fecha correcta se lanzará una excepción.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar
+     ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+"""
+
+fec_01 = "10/12/2024"
+fec_02 = "12/09/2022"
+
+fec_01 = fec_01.split('/')
+print(fec_01)
+
+an = int(fec_01[2])
+if an % 400 == 0 or (an % 4 == 0 and an % 100 != 0):
+    print('bisiesto')
+else:
+    print('no bisiesto')
+
+mes = {'01': '31', '02': '28', '03': '31'}
---
+++ cuantos_dias/practice.py
@@ -0,0 +1,22 @@
+# dna = input()
+# c = []
+# for i in dna:
+#     if i == "A":
+#         c.append(i * 2)
+#     elif i == "T":
+#         c.append(i * 3)
+#     elif i == 'G':
+#         pass
+#     elif i == 'C':
+#         c.append(i)
+#     else:
+#         c.append(i)
+# c = ''.join(c)
+# print(c)
+#
+# lst = [i for i in dna]
+# print(lst)
+
+lsta = ['A', 'T', 'G', 'C']
+for i in lsta:
+    print(i)
