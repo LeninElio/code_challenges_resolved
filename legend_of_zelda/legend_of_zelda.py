2782cb824fd364c1450c4930e842d9d82e6c0b8a
LeninElio <moreno_leninelio@hotmail.es>
2023-03-25 20:12:12 -0500

    legends of zelda

--- legend_of_zelda/legend_of_zelda.py
+++ legend_of_zelda/legend_of_zelda.py
@@ -23,3 +23,42 @@
  */
 
 """
+import pandas as pd
+from datetime import datetime
+import locale
+
+# Nota (Lenin Elio - 25/03/2023 16:12)
+# Establecemos la configuración regional a español
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+
+# Cargamos los datos del archivo CSV
+data = pd.read_csv('data_legends.csv')
+
+# Pedimos al usuario que seleccione los dos juegos
+juego1 = input('Ingresa el nombre del primer juego: ')
+juego2 = input('Ingresa el nombre del segundo juego: ')
+
+# Nota (Lenin Elio - 25/03/2023 16:17)
+# Obtenemos las fechas de lanzamiento de los juegos seleccionados
+try:
+    fecha1 = data.loc[data['nombre_juego'] == juego1, 'fecha_lanzamiento'].values[0]
+    fecha2 = data.loc[data['nombre_juego'] == juego2, 'fecha_lanzamiento'].values[0]
+except IndexError:
+    fecha1, fecha2 = 0, 0
+
+if fecha1 != 0 or fecha2 != 0:
+    # Convertimos las fechas a objetos datetime
+    fecha1 = datetime.strptime(fecha1, '%d de %B de %Y')
+    fecha2 = datetime.strptime(fecha2, '%d de %B de %Y')
+
+    # Calculamos la diferencia en días entre las dos fechas
+    diferencia = abs((fecha2 - fecha1).days)
+
+    # Calculamos cuántos años y días hay en la diferencia
+    years = diferencia // 365
+    days = diferencia % 365
+
+    print(f'Hay {years} años y {days} días entre {juego1} y {juego2}.')
+
+else:
+    print('Juego no encontrado.')
